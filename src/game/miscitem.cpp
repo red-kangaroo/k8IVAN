2e2eec4b8a0b74fd6d565a93f99e4e99636681a9
ketmar <ketmar@ketmar.no-ip.org>
2010-09-05 20:49:04 +0300

    less warnings

--- src/game/miscitem.cpp
+++ src/game/miscitem.cpp
@@ -1603,49 +1603,38 @@ truth mine::CheckPickUpEffect(character*)
   return true;
 }
 
-void scrollofrepair::FinishReading(character* Reader)
-{
-  for(;;)
-  {
-    itemvector Item;
 
-    if(!Reader->SelectFromPossessions(Item, CONST_S("Which item do you wish to repair?"), NO_MULTI_SELECT|SELECT_PAIR, &item::IsRepairable))
+void scrollofrepair::FinishReading (character *Reader) {
+  for (;;) {
+    itemvector Item;
+    if (!Reader->SelectFromPossessions(Item, CONST_S("Which item do you wish to repair?"), NO_MULTI_SELECT|SELECT_PAIR, &item::IsRepairable))
     {
       ADD_MESSAGE("You notice you haven't got anything to repair.");
       return;
     }
-
-    if(!Item.empty())
-    {
-      if(Item[0]->HandleInPairs() && Item.size() == 1)
-      {
-  ADD_MESSAGE("Only one %s will be repaired.", Item[0]->CHAR_NAME(UNARTICLED));
-
-  if(!game::TruthQuestion(CONST_S("Still continue? [y/N]")))
-    continue;
+    if (!Item.empty()) {
+      if (Item[0]->HandleInPairs() && Item.size() == 1) {
+        ADD_MESSAGE("Only one %s will be repaired.", Item[0]->CHAR_NAME(UNARTICLED));
+        if (!game::TruthQuestion(CONST_S("Still continue? [y/N]"))) continue;
       }
-
-      if(Item.size() == 1)
-  ADD_MESSAGE("As you read the scroll, %s glows green and %s.", Item[0]->CHAR_NAME(DEFINITE), Item[0]->IsBroken() ? "fixes itself" : "its rust vanishes");
+      if (Item.size() == 1)
+        ADD_MESSAGE("As you read the scroll, %s glows green and %s.", Item[0]->CHAR_NAME(DEFINITE), Item[0]->IsBroken() ? "fixes itself" : "its rust vanishes");
       else
-  ADD_MESSAGE("As you read the scroll, %s glow green and %s.", Item[0]->CHAR_NAME(PLURAL), Item[0]->IsBroken() ? "fix themselves" : "their rust vanishes");
-
-      for(uint c = 0; c < Item.size(); ++c)
-      {
-  Item[c]->RemoveRust();
-  Item[c]->Fix();
+        ADD_MESSAGE("As you read the scroll, %s glow green and %s.", Item[0]->CHAR_NAME(PLURAL), Item[0]->IsBroken() ? "fix themselves" : "their rust vanishes");
+      for (uint c = 0; c < Item.size(); ++c) {
+        Item[c]->RemoveRust();
+        Item[c]->Fix();
       }
-
       break;
+    } else {
+      if (game::TruthQuestion(CONST_S("Really cancel read? [y/N]"))) return;
     }
-    else if(game::TruthQuestion(CONST_S("Really cancel read? [y/N]")))
-      return;
   }
-
   RemoveFromSlot();
   SendToHell();
   Reader->EditExperience(INTELLIGENCE, 300, 1 << 12);
 }
+
 
 item* brokenbottle::Fix()
 {
