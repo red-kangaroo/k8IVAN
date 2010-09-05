a20ba5f4e0a781d2017c2f64515a43b6a234267e
ketmar <ketmar@ketmar.no-ip.org>
2010-09-05 10:48:34 +0300

    removed memory leak

--- src/felib/festring.h
+++ src/felib/festring.h
@@ -29,7 +29,7 @@ public:
   festring () : Data(0), Size(0), OwnsData(false), Reserved(0) {}
   explicit festring (sizetype);
   festring (sizetype, char);
-  festring (cchar *CStr) : Data(0/*const_cast<char *>(CStr)*/), Size(strlen(CStr)), OwnsData(false), Reserved(0) {
+  festring (cchar *CStr) : Data(0/*const_cast<char *>(CStr)*/), Size(strlen(CStr)), OwnsData(true), Reserved(0) {
     const char *s = const_cast<char *>(CStr);
     int N = s ? strlen(s) : 0;
     char *Ptr = 4+new char[Reserved+5+N];
@@ -39,7 +39,7 @@ public:
       if (s) memcpy(Data, s, N); else memset(Data, 0, N);
     }
   }
-  festring (cchar *CStr, sizetype N) : Data(0/*const_cast<char *>(CStr)*/), Size(N), OwnsData(false), Reserved(0) {
+  festring (cchar *CStr, sizetype N) : Data(0/*const_cast<char *>(CStr)*/), Size(N), OwnsData(true), Reserved(0) {
     const char *s = const_cast<char *>(CStr);
     char *Ptr = 4+new char[Reserved+5+N];
     REFS(Ptr) = 0;
