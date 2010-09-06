6b99a72344a87b03dd51549b11daa133160fad92
ketmar <ketmar@ketmar.no-ip.org>
2010-09-07 02:32:09 +0300

    restyling

--- src/game/worldmap.cpp
+++ src/game/worldmap.cpp
@@ -223,392 +223,285 @@ void worldmap::Generate () {
 }
 
 
-void worldmap::RandomizeAltitude()
-{
+void worldmap::RandomizeAltitude () {
   game::BusyAnimation();
-
-  for(int x = 0; x < XSize; ++x)
-    for(int y = 0; y < YSize; ++y)
+  for (int x = 0; x < XSize; ++x)
+    for (int y = 0; y < YSize; ++y)
       AltitudeBuffer[x][y] = 4000 - RAND() % 8000;
 }
 
-void worldmap::SmoothAltitude()
-{
-  for(int c = 0; c < 10; ++c)
-  {
+
+void worldmap::SmoothAltitude () {
+  for (int c = 0; c < 10; ++c) {
     game::BusyAnimation();
     int x, y;
-
-    for(y = 0; y < YSize; ++y)
-      SafeSmooth(0, y);
-
-    for(x = 1; x < XSize - 1; ++x)
-    {
+    for (y = 0; y < YSize; ++y) SafeSmooth(0, y);
+    for (x = 1; x < XSize - 1; ++x) {
       SafeSmooth(x, 0);
-
-      for(y = 1; y < YSize - 1; ++y)
-  FastSmooth(x, y);
-
+      for (y = 1; y < YSize - 1; ++y) FastSmooth(x, y);
       SafeSmooth(x, YSize - 1);
     }
-
-    for(y = 0; y < YSize; ++y)
-      SafeSmooth(XSize - 1, y);
+    for (y = 0; y < YSize; ++y) SafeSmooth(XSize - 1, y);
   }
 }
 
-void worldmap::FastSmooth(int x, int y)
-{
+
+void worldmap::FastSmooth (int x, int y) {
   long HeightNear = 0;
   int d;
-
-  for(d = 0; d < 4; ++d)
-    HeightNear += OldAltitudeBuffer[x + DirX[d]][y + DirY[d]];
-
-  for(d = 4; d < 8; ++d)
-    HeightNear += AltitudeBuffer[x + DirX[d]][y + DirY[d]];
-
+  for (d = 0; d < 4; ++d) HeightNear += OldAltitudeBuffer[x + DirX[d]][y + DirY[d]];
+  for (d = 4; d < 8; ++d) HeightNear += AltitudeBuffer[x + DirX[d]][y + DirY[d]];
   OldAltitudeBuffer[x][y] = AltitudeBuffer[x][y];
   AltitudeBuffer[x][y] = HeightNear >> 3;
 }
 
-void worldmap::SafeSmooth(int x, int y)
-{
+
+void worldmap::SafeSmooth (int x, int y) {
   long HeightNear = 0;
   int d, SquaresNear = 0;
-
-  for(d = 0; d < 4; ++d)
-  {
+  for (d = 0; d < 4; ++d) {
     int X = x + DirX[d];
     int Y = y + DirY[d];
-
-    if(IsValidPos(X, Y))
-    {
+    if (IsValidPos(X, Y)) {
       HeightNear += OldAltitudeBuffer[X][Y];
       ++SquaresNear;
     }
   }
-
-  for(d = 4; d < 8; ++d)
-  {
+  for (d = 4; d < 8; ++d) {
     int X = x + DirX[d];
     int Y = y + DirY[d];
-
-    if(IsValidPos(X, Y))
-    {
+    if (IsValidPos(X, Y)) {
       HeightNear += AltitudeBuffer[X][Y];
       ++SquaresNear;
     }
   }
-
   OldAltitudeBuffer[x][y] = AltitudeBuffer[x][y];
   AltitudeBuffer[x][y] = HeightNear / SquaresNear;
 }
 
-void worldmap::GenerateClimate()
-{
+
+void worldmap::GenerateClimate () {
   game::BusyAnimation();
-
-  for(int y = 0; y < YSize; ++y)
-  {
+  for (int y = 0; y < YSize; ++y) {
     double DistanceFromEquator = fabs(double(y) / YSize - 0.5);
-    truth LatitudeRainy = DistanceFromEquator <= 0.05
-        || (DistanceFromEquator > 0.25
-            && DistanceFromEquator <= 0.45);
-
-    for(int x = 0; x < XSize; ++x)
-    {
-      if(AltitudeBuffer[x][y] <= 0)
-      {
-  TypeBuffer[x][y] = OceanType;
-  continue;
+    truth LatitudeRainy = DistanceFromEquator <= 0.05 || (DistanceFromEquator > 0.25 && DistanceFromEquator <= 0.45);
+    for (int x = 0; x < XSize; ++x) {
+      if (AltitudeBuffer[x][y] <= 0) {
+        TypeBuffer[x][y] = OceanType;
+        continue;
       }
-
       truth Rainy = LatitudeRainy;
-
-      if(!Rainy)
-  for(int d = 0; d < 8; ++d)
-  {
-    v2 Pos = v2(x, y) + game::GetMoveVector(d);
-
-    if(IsValidPos(Pos) && AltitudeBuffer[Pos.X][Pos.Y] <= 0)
-    {
-      Rainy = true;
-      break;
-    }
-  }
-
-      int Temperature = int(MAX_TEMPERATURE
-          - DistanceFromEquator * LATITUDE_EFFECT
-          - AltitudeBuffer[x][y] * ALTITUDE_EFFECT);
+      if (!Rainy) {
+        for(int d = 0; d < 8; ++d) {
+          v2 Pos = v2(x, y) + game::GetMoveVector(d);
+          if (IsValidPos(Pos) && AltitudeBuffer[Pos.X][Pos.Y] <= 0) {
+            Rainy = true;
+            break;
+          }
+        }
+      }
+      int Temperature = int(MAX_TEMPERATURE-DistanceFromEquator*LATITUDE_EFFECT-AltitudeBuffer[x][y]*ALTITUDE_EFFECT);
       int Type = 0;
-
-      if(Temperature <= COLD)
-  Type = Rainy ? SnowType : GlacierType;
-      else if(Temperature <= MEDIUM)
-  Type = Rainy ? EGForestType : SnowType;
-      else if(Temperature <= WARM)
-  Type = Rainy ? LForestType : SteppeType;
-      else if(Temperature <= HOT)
-  Type = Rainy ? LForestType : DesertType;
-      else
-  Type = Rainy ? JungleType : DesertType;
-
+      if (Temperature <= COLD) Type = Rainy ? SnowType : GlacierType;
+      else if (Temperature <= MEDIUM) Type = Rainy ? EGForestType : SnowType;
+      else if (Temperature <= WARM) Type = Rainy ? LForestType : SteppeType;
+      else if (Temperature <= HOT) Type = Rainy ? LForestType : DesertType;
+      else Type = Rainy ? JungleType : DesertType;
       TypeBuffer[x][y] = Type;
     }
   }
 }
 
-void worldmap::SmoothClimate()
-{
-  for(int c = 0; c < 3; ++c)
-  {
+
+void worldmap::SmoothClimate () {
+  for (int c = 0; c < 3; ++c) {
     game::BusyAnimation();
-
-    for(int x = 0; x < XSize; ++x)
-      for(int y = 0; y < YSize; ++y)
-  if((OldTypeBuffer[x][y] = TypeBuffer[x][y]) != OceanType)
-    TypeBuffer[x][y] = WhatTerrainIsMostCommonAroundCurrentTerritorySquareIncludingTheSquareItself(x, y);
+    for (int x = 0; x < XSize; ++x)
+      for (int y = 0; y < YSize; ++y)
+        if ((OldTypeBuffer[x][y] = TypeBuffer[x][y]) != OceanType)
+          TypeBuffer[x][y] = WhatTerrainIsMostCommonAroundCurrentTerritorySquareIncludingTheSquareItself(x, y);
   }
-
   game::BusyAnimation();
-
-  for(int x = 0; x < XSize; ++x)
-    for(int y = 0; y < YSize; ++y)
+  for (int x = 0; x < XSize; ++x)
+    for (int y = 0; y < YSize; ++y)
       Map[x][y]->ChangeGWTerrain
   (protocontainer<gwterrain>::GetProto(TypeBuffer[x][y])->Spawn());
 }
 
-/* Evil... */
 
-#define ANALYZE_TYPE(type)\
-{\
+/* Evil... */
+#define ANALYZE_TYPE(type) {\
   int T = type;\
-  \
-  for(c = 0; c < u; ++c)\
-    if(T == UsedType[c])\
-      {\
-  ++TypeAmount[c];\
-  break;\
-      }\
-  \
-  if(c == u)\
-    {\
-      UsedType[u] = T;\
-      TypeAmount[u++] = 1;\
-    }\
+  for (c = 0; c < u; ++c) if (T == UsedType[c]) { ++TypeAmount[c]; break; }\
+  if (c == u) { UsedType[u] = T; TypeAmount[u++] = 1; }\
 }
 
-int worldmap::WhatTerrainIsMostCommonAroundCurrentTerritorySquareIncludingTheSquareItself(int x, int y)
-{
+
+/* k8: WOW! */
+int worldmap::WhatTerrainIsMostCommonAroundCurrentTerritorySquareIncludingTheSquareItself (int x, int y) {
   int UsedType[9];
   int TypeAmount[9];
   int c, d, u = 1;
-
   UsedType[0] = TypeBuffer[x][y];
   TypeAmount[0] = 1;
-
-  for(d = 0; d < 4; ++d)
-  {
-    int X = x + DirX[d];
-    int Y = y + DirY[d];
-
-    if(IsValidPos(X, Y))
-      ANALYZE_TYPE(OldTypeBuffer[X][Y]);
+  for (d = 0; d < 4; ++d) {
+    int X = x+DirX[d];
+    int Y = y+DirY[d];
+    if (IsValidPos(X, Y)) ANALYZE_TYPE(OldTypeBuffer[X][Y]);
   }
-
-  for(d = 4; d < 8; ++d)
-  {
-    int X = x + DirX[d];
-    int Y = y + DirY[d];
-
-    if(IsValidPos(X, Y))
-      ANALYZE_TYPE(TypeBuffer[X][Y]);
+  for (d = 4; d < 8; ++d) {
+    int X = x+DirX[d];
+    int Y = y+DirY[d];
+    if (IsValidPos(X, Y)) ANALYZE_TYPE(TypeBuffer[X][Y]);
   }
-
   int MostCommon = 0;
-
-  for(c = 1; c < u; ++c)
-    if(TypeAmount[c] > TypeAmount[MostCommon] && UsedType[c] != OceanType)
-      MostCommon = c;
-
+  for (c = 1; c < u; ++c) if (TypeAmount[c] > TypeAmount[MostCommon] && UsedType[c] != OceanType) MostCommon = c;
   return UsedType[MostCommon];
 }
 
-void worldmap::CalculateContinents()
-{
-  uint c;
 
-  for(c = 1; c < Continent.size(); ++c)
-    delete Continent[c];
-
+void worldmap::CalculateContinents () {
+  for (uint c = 1; c < Continent.size(); ++c) delete Continent[c];
   Continent.resize(1, 0);
-  memset(ContinentBuffer[0], 0, XSizeTimesYSize * sizeof(uchar));
+  memset(ContinentBuffer[0], 0, XSizeTimesYSize*sizeof(uchar));
   game::BusyAnimation();
-
-  for(int x = 0; x < XSize; ++x)
-    for(int y = 0; y < YSize; ++y)
-      if(AltitudeBuffer[x][y] > 0)
-      {
-  truth Attached = false;
-
-  for (int d = 0; d < 8; ++d) {
-    v2 Pos = v2(x, y)+game::GetMoveVector(d);
-    if (IsValidPos(Pos)) {
-      cint NearCont = ContinentBuffer[Pos.X][Pos.Y];
-      if (NearCont) {
-        cint ThisCont = ContinentBuffer[x][y];
-        if (ThisCont) {
-          if (ThisCont != NearCont) {
-            if (Continent[ThisCont]->GetSize() < Continent[NearCont]->GetSize())
-              Continent[ThisCont]->AttachTo(Continent[NearCont]);
-            else
-              Continent[NearCont]->AttachTo(Continent[ThisCont]);
+  for (int x = 0; x < XSize; ++x) {
+    for (int y = 0; y < YSize; ++y) {
+      if (AltitudeBuffer[x][y] > 0) {
+        truth Attached = false;
+        for (int d = 0; d < 8; ++d) {
+          v2 Pos = v2(x, y)+game::GetMoveVector(d);
+          if (IsValidPos(Pos)) {
+            cint NearCont = ContinentBuffer[Pos.X][Pos.Y];
+            if (NearCont) {
+              cint ThisCont = ContinentBuffer[x][y];
+              if (ThisCont) {
+                if (ThisCont != NearCont) {
+                  if (Continent[ThisCont]->GetSize() < Continent[NearCont]->GetSize())
+                    Continent[ThisCont]->AttachTo(Continent[NearCont]);
+                  else
+                    Continent[NearCont]->AttachTo(Continent[ThisCont]);
+                }
+              } else Continent[NearCont]->Add(v2(x, y));
+              Attached = true;
+            }
           }
-        } else Continent[NearCont]->Add(v2(x, y));
-        Attached = true;
+        }
+        if (!Attached) {
+          if (Continent.size() == 255) {
+            RemoveEmptyContinents();
+            if (Continent.size() == 255) ABORT("Valpurus shall not carry more continents!");
+          }
+          continent *NewContinent = new continent(Continent.size());
+          NewContinent->Add(v2(x, y));
+          Continent.push_back(NewContinent);
+        }
       }
     }
   }
-
-  if(!Attached)
-  {
-    if(Continent.size() == 255)
-    {
-      RemoveEmptyContinents();
-
-      if(Continent.size() == 255)
-        ABORT("Valpurus shall not carry more continents!");
-    }
-
-    continent* NewContinent = new continent(Continent.size());
-    NewContinent->Add(v2(x, y));
-    Continent.push_back(NewContinent);
-  }
-      }
-
   RemoveEmptyContinents();
-
-  for(c = 1; c < Continent.size(); ++c)
-    Continent[c]->GenerateInfo();
+  for (uint c = 1; c < Continent.size(); ++c) Continent[c]->GenerateInfo();
 }
 
-void worldmap::RemoveEmptyContinents()
-{
-  for(uint c = 1; c < Continent.size(); ++c)
-    if(!Continent[c]->GetSize())
-      for(uint i = Continent.size() - 1; i >= c; i--)
-  if(Continent[i]->GetSize())
-  {
-    Continent[i]->AttachTo(Continent[c]);
-    delete Continent[i];
-    Continent.pop_back();
-    break;
-  }
-  else
-  {
-    delete Continent[i];
-    Continent.pop_back();
+
+void worldmap::RemoveEmptyContinents () {
+  for (uint c = 1; c < Continent.size(); ++c) {
+    if (!Continent[c]->GetSize()) {
+      for (uint i = Continent.size() - 1; i >= c; i--) {
+        if (Continent[i]->GetSize()) {
+          Continent[i]->AttachTo(Continent[c]);
+          delete Continent[i];
+          Continent.pop_back();
+          break;
+        } else {
+          delete Continent[i];
+          Continent.pop_back();
+        }
+      }
+    }
   }
 }
 
-void worldmap::Draw(truth) const
-{
+
+void worldmap::Draw (truth) const {
   cint XMin = Max(game::GetCamera().X, 0);
   cint YMin = Max(game::GetCamera().Y, 0);
-  cint XMax = Min(XSize, game::GetCamera().X + game::GetScreenXSize());
-  cint YMax = Min(YSize, game::GetCamera().Y + game::GetScreenYSize());
-  blitdata BlitData = { DOUBLE_BUFFER,
-      { 0, 0 },
-      { 0, 0 },
-      { TILE_SIZE, TILE_SIZE },
-      { 0 },
-      TRANSPARENT_COLOR,
-      ALLOW_ANIMATE|ALLOW_ALPHA };
-
-  if(!game::GetSeeWholeMapCheatMode())
-  {
-    for(int x = XMin; x < XMax; ++x)
-    {
+  cint XMax = Min(XSize, game::GetCamera().X+game::GetScreenXSize());
+  cint YMax = Min(YSize, game::GetCamera().Y+game::GetScreenYSize());
+  blitdata BlitData = {
+    DOUBLE_BUFFER,
+    { 0, 0 },
+    { 0, 0 },
+    { TILE_SIZE, TILE_SIZE },
+    { 0 },
+    TRANSPARENT_COLOR,
+    ALLOW_ANIMATE|ALLOW_ALPHA
+  };
+  if (!game::GetSeeWholeMapCheatMode()) {
+    for (int x = XMin; x < XMax; ++x) {
       BlitData.Dest = game::CalculateScreenCoordinates(v2(x, YMin));
-      wsquare** Square = &Map[x][YMin];
-
-      for(int y = YMin; y < YMax;
-    ++y, ++Square, BlitData.Dest.Y += TILE_SIZE)
-  if((*Square)->LastSeen)
-    (*Square)->Draw(BlitData);
+      wsquare **Square = &Map[x][YMin];
+      for (int y = YMin; y < YMax; ++y, ++Square, BlitData.Dest.Y += TILE_SIZE) {
+        if ((*Square)->LastSeen) (*Square)->Draw(BlitData);
+      }
     }
-  }
-  else
-  {
-    for(int x = XMin; x < XMax; ++x)
-    {
+  } else {
+    for (int x = XMin; x < XMax; ++x) {
       BlitData.Dest = game::CalculateScreenCoordinates(v2(x, YMin));
-      wsquare** Square = &Map[x][YMin];
-
-      for(int y = YMin; y < YMax;
-    ++y, ++Square, BlitData.Dest.Y += TILE_SIZE)
-  (*Square)->Draw(BlitData);
+      wsquare **Square = &Map[x][YMin];
+      for (int y = YMin; y < YMax; ++y, ++Square, BlitData.Dest.Y += TILE_SIZE) (*Square)->Draw(BlitData);
     }
   }
 }
 
-void worldmap::CalculateLuminances()
-{
-  for(ulong c = 0; c < XSizeTimesYSize; ++c)
-    Map[0][c]->CalculateLuminance();
+
+void worldmap::CalculateLuminances () {
+  for (ulong c = 0; c < XSizeTimesYSize; ++c) Map[0][c]->CalculateLuminance();
 }
 
-void worldmap::CalculateNeighbourBitmapPoses()
-{
-  for(ulong c = 0; c < XSizeTimesYSize; ++c)
-    Map[0][c]->GetGWTerrain()->CalculateNeighbourBitmapPoses();
+
+void worldmap::CalculateNeighbourBitmapPoses () {
+  for (ulong c = 0; c < XSizeTimesYSize; ++c) Map[0][c]->GetGWTerrain()->CalculateNeighbourBitmapPoses();
 }
 
-wsquare* worldmap::GetNeighbourWSquare(v2 Pos, int I) const
-{
+
+wsquare *worldmap::GetNeighbourWSquare (v2 Pos, int I) const {
   Pos += game::GetMoveVector(I);
-
-  if(Pos.X >= 0 && Pos.Y >= 0 && Pos.X < XSize && Pos.Y < YSize)
-    return Map[Pos.X][Pos.Y];
-  else
-    return 0;
+  if (Pos.X >= 0 && Pos.Y >= 0 && Pos.X < XSize && Pos.Y < YSize) return Map[Pos.X][Pos.Y];
+  return 0;
 }
 
-void worldmap::RevealEnvironment(v2 Pos, int Radius)
-{
+
+void worldmap::RevealEnvironment (v2 Pos, int Radius) {
   rect Rect;
   femath::CalculateEnvironmentRectangle(Rect, Border, Pos, Radius);
-
-  for(int x = Rect.X1; x <= Rect.X2; ++x)
-    for(int y = Rect.Y1; y <= Rect.Y2; ++y)
+  for (int x = Rect.X1; x <= Rect.X2; ++x)
+    for (int y = Rect.Y1; y <= Rect.Y2; ++y)
       Map[x][y]->SignalSeen();
 }
 
-outputfile& operator<<(outputfile& SaveFile, const worldmap* WorldMap)
-{
+
+outputfile &operator << (outputfile &SaveFile, const worldmap *WorldMap) {
   WorldMap->Save(SaveFile);
   return SaveFile;
 }
 
-inputfile& operator>>(inputfile& SaveFile, worldmap*& WorldMap)
-{
+
+inputfile &operator >> (inputfile &SaveFile, worldmap *&WorldMap) {
   WorldMap = new worldmap;
   WorldMap->Load(SaveFile);
   return SaveFile;
 }
 
-void worldmap::UpdateLOS()
-{
+
+void worldmap::UpdateLOS () {
   game::RemoveLOSUpdateRequest();
   int Radius = PLAYER->GetLOSRange();
-  long RadiusSquare = Radius * Radius;
+  long RadiusSquare = Radius*Radius;
   v2 Pos = PLAYER->GetPos();
   rect Rect;
   femath::CalculateEnvironmentRectangle(Rect, Border, Pos, Radius);
-
-  for(int x = Rect.X1; x <= Rect.X2; ++x)
-    for(int y = Rect.Y1; y <= Rect.Y2; ++y)
-      if(long(HypotSquare(Pos.X - x, Pos.Y - y)) <= RadiusSquare)
-  Map[x][y]->SignalSeen();
+  for (int x = Rect.X1; x <= Rect.X2; ++x)
+    for (int y = Rect.Y1; y <= Rect.Y2; ++y)
+      if (long(HypotSquare(Pos.X-x, Pos.Y-y)) <= RadiusSquare) Map[x][y]->SignalSeen();
 }
