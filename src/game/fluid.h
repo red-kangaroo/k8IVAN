e5ee67fc86fddc4c188d7e3587589f1b992a0b3d
ketmar <ketmar@ketmar.no-ip.org>
2010-09-25 23:53:33 +0300

    some restyling

--- src/game/fluid.h
+++ src/game/fluid.h
@@ -9,80 +9,82 @@
  *  along with this file for more details
  *
  */
-
 #ifndef __FLUID_H__
 #define __FLUID_H__
 
 #include "lsquare.h"
 #include "trap.h"
 
+
 class bitmap;
 
-typedef truth (rawbitmap::*pixelpredicate)(v2) const;
 
-class fluid : public entity
-{
- public:
+typedef truth (rawbitmap::*pixelpredicate) (v2) const;
+
+class fluid : public entity {
+public:
   /* Come To The Dark Side */
-  fluid* Next;
- public:
-  fluid();
-  fluid(liquid*, lsquare*);
-  fluid(liquid*, item*, cfestring&, truth);
-  virtual ~fluid();
-  virtual void Be();
-  void Save(outputfile&) const;
-  void Load(inputfile&);
-  void SimpleDraw(blitdata&) const;
-  void Draw(blitdata&) const;
-  liquid* GetLiquid() const { return Liquid; }
-  virtual square* GetSquareUnderEntity(int = 0) const { return LSquareUnder; }
-  square* GetSquareUnder() const { return LSquareUnder; }
-  void SetLSquareUnder(lsquare* What) { LSquareUnder = What; }
-  lsquare* GetLSquareUnder() const { return LSquareUnder; }
-  virtual truth IsOnGround() const { return true; }
-  void AddLiquid(long);
-  void AddLiquidAndVolume(long);
-  virtual void SignalVolumeAndWeightChange();
-  void SetMotherItem(item*);
-  static void AddFluidInfo(const fluid*, festring&);
-  void CheckGearPicture(v2, int, truth);
-  void DrawGearPicture(blitdata&, int) const;
-  truth FadePictures();
-  void DrawBodyArmorPicture(blitdata&, int) const;
-  void Redistribute();
-  virtual material* RemoveMaterial(material*);
-  void Destroy();
-  cfestring& GetLocationName() const { return LocationName; }
-  truth IsInside() const { return Flags & FLUID_INSIDE; }
+  fluid *Next;
+
+public:
+  fluid ();
+  fluid (liquid *, lsquare *);
+  fluid (liquid *, item *, cfestring &, truth);
+  virtual ~fluid ();
+
+  virtual void Be ();
+  void Save (outputfile &) const;
+  void Load (inputfile &);
+  void SimpleDraw (blitdata &) const;
+  void Draw (blitdata &) const;
+  inline liquid *GetLiquid () const { return Liquid; }
+  virtual square *GetSquareUnderEntity (int = 0) const { return LSquareUnder; }
+  inline square *GetSquareUnder () const { return LSquareUnder; }
+  inline void SetLSquareUnder (lsquare *What) { LSquareUnder = What; }
+  inline lsquare *GetLSquareUnder () const { return LSquareUnder; }
+  virtual truth IsOnGround () const { return true; }
+  void AddLiquid (long);
+  void AddLiquidAndVolume (long);
+  virtual void SignalVolumeAndWeightChange ();
+  void SetMotherItem (item *);
+  static void AddFluidInfo (const fluid *, festring &);
+  void CheckGearPicture (v2, int, truth);
+  void DrawGearPicture (blitdata &, int) const;
+  truth FadePictures ();
+  void DrawBodyArmorPicture (blitdata &, int) const;
+  void Redistribute ();
+  virtual material *RemoveMaterial (material *);
+  void Destroy ();
+  inline cfestring &GetLocationName () const { return LocationName; }
+  inline truth IsInside () const { return Flags & FLUID_INSIDE; }
   truth UseImage() const;
-  virtual int GetTrapType() const { return Liquid->GetType() | FLUID_TRAP; }
-  virtual ulong GetTrapID() const { return TrapData.TrapID; }
-  virtual ulong GetVictimID() const { return TrapData.VictimID; }
-  virtual void AddTrapName(festring&, int) const;
-  virtual void UnStick() { TrapData.VictimID = 0; }
-  virtual void UnStick(int I) { TrapData.BodyParts &= ~(1 << I); }
-  void StepOnEffect(character*);
-  virtual truth TryToUnStick(character*, v2);
-  virtual void PreProcessForBone();
-  virtual void PostProcessForBone();
-  virtual truth IsStuckTo(ccharacter*) const;
-  truth IsDangerous(ccharacter*) const;
- protected:
+  virtual int GetTrapType () const { return Liquid->GetType() | FLUID_TRAP; }
+  virtual ulong GetTrapID () const { return TrapData.TrapID; }
+  virtual ulong GetVictimID () const { return TrapData.VictimID; }
+  virtual void AddTrapName (festring &, int) const;
+  virtual void UnStick () { TrapData.VictimID = 0; }
+  virtual void UnStick (int I) { TrapData.BodyParts &= ~(1 << I); }
+  void StepOnEffect (character *);
+  virtual truth TryToUnStick (character *, v2);
+  virtual void PreProcessForBone ();
+  virtual void PostProcessForBone ();
+  virtual truth IsStuckTo (ccharacter *) const;
+  truth IsDangerous (ccharacter *) const;
+
+protected:
   trapdata TrapData;
-  struct imagedata
-  {
-    imagedata(truth = true);
-    ~imagedata();
-    void Animate(blitdata&, int) const;
-    void AddLiquidToPicture(const rawbitmap*, long, long, col16, pixelpredicate);
-    void Save(outputfile&) const;
-    void Load(inputfile&);
-    truth Fade();
-    void Clear(truth);
+  struct imagedata {
+    imagedata (truth = true);
+    ~imagedata ();
+    void Animate (blitdata &, int) const;
+    void AddLiquidToPicture (const rawbitmap *, long, long, col16, pixelpredicate);
+    void Save (outputfile &) const;
+    void Load (inputfile &);
+    truth Fade ();
+    void Clear (truth);
     /* Only pictures of fluids not on ground have their RandMaps initialized,
        since they are animated. Note that the picture is always unrotated. */
-    bitmap* Picture;
+    bitmap *Picture;
     /* Used by Animate() */
     mutable int DripTimer;
     mutable v2 DripPos;
@@ -104,22 +106,24 @@ class fluid : public entity
        in the left hand of a character. */
     int SpecialFlags;
   };
-  liquid* Liquid;
-  lsquare* LSquareUnder;
+  liquid *Liquid;
+  lsquare *LSquareUnder;
   /* MotherItem == 0 means that the fluid is on ground */
-  item* MotherItem;
+  item *MotherItem;
   imagedata Image;
   /* Data of pictures shown over the player if he equips the item which
      the fluid covers. Note that these are not destroyed when the armor
      or weapon is unequipped. There is no real need, since the existence
      of the fluid is very temporary anyway. */
-  imagedata* GearImage;
+  imagedata *GearImage;
   ulong Flags;
   festring LocationName;
   static const long BodyArmorPartPixels[];
 };
 
-outputfile& operator<<(outputfile&, const fluid*);
-inputfile& operator>>(inputfile&, fluid*&);
+
+outputfile &operator << (outputfile &, const fluid *);
+inputfile &operator >> (inputfile &, fluid *&);
+
 
 #endif
