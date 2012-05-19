31410e5c7c0bc0f38258b1cb0a434f91472de4d0
ketmar <ketmar@ketmar.no-ip.org>
2012-05-19 23:58:27 +0300

    cosmetix

--- src/game/command.cpp
+++ src/game/command.cpp
@@ -277,11 +277,14 @@ truth commandsystem::Open (character *Char) {
   if (Char->CanOpen()) {
     int Key;
     truth OpenableItems = Char->GetStack()->SortedItems(Char, &item::IsOpenable);
-    if (OpenableItems)
+    //
+    if (OpenableItems) {
       Key = game::AskForKeyPress(CONST_S("What do you wish to open? [press a direction key, space or i]"));
-    else
+    } else {
       Key = game::AskForKeyPress(CONST_S("What do you wish to open? [press a direction key or space]"));
-    if ((Key == 'i' || Key == 'I') && OpenableItems) {
+    }
+    //
+    if (OpenableItems && (Key == 'i' || Key == 'I')) {
       item *Item = Char->GetStack()->DrawContents(Char, CONST_S("What do you want to open?"), 0, &item::IsOpenable);
       return Item && Item->Open(Char);
     }
