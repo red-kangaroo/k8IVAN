77d6ee628e5aaf959a54135c09a1d78472af9caf
ketmar <ketmar@ketmar.no-ip.org>
2012-05-20 23:28:46 +0300

    better include order for separations

--- src/game/bodyparts/0list.cpp
+++ src/game/bodyparts/0list.cpp
@@ -36,34 +36,44 @@ inputfile &operator >> (inputfile &SaveFile, scar &Scar) {
 
 
 #include "bodyparts/item_bodypart.cpp"
-#include "bodyparts/bodypart_head.cpp"
-#include "bodyparts/bodypart_torso.cpp"
-#include "bodyparts/torso_normaltorso.cpp"
-#include "bodyparts/torso_humanoidtorso.cpp"
-#include "bodyparts/bodypart_arm.cpp"
-#include "bodyparts/arm_rightarm.cpp"
-#include "bodyparts/arm_leftarm.cpp"
-#include "bodyparts/bodypart_groin.cpp"
-#include "bodyparts/bodypart_leg.cpp"
-#include "bodyparts/leg_rightleg.cpp"
-#include "bodyparts/leg_leftleg.cpp"
 #include "bodyparts/item_corpse.cpp"
-#include "bodyparts/normaltorso_eddytorso.cpp"
-#include "bodyparts/normaltorso_largetorso.cpp"
-#include "bodyparts/corpse_largecorpse.cpp"
+
+#include "bodyparts/bodypart_arm.cpp"
+#include "bodyparts/bodypart_groin.cpp"
+#include "bodyparts/bodypart_head.cpp"
+#include "bodyparts/bodypart_leg.cpp"
+#include "bodyparts/bodypart_torso.cpp"
+
 #include "bodyparts/head_ennerhead.cpp"
 #include "bodyparts/head_playerkindhead.cpp"
-#include "bodyparts/humanoidtorso_playerkindtorso.cpp"
-#include "bodyparts/rightarm_playerkindrightarm.cpp"
+
+#include "bodyparts/arm_leftarm.cpp"
+#include "bodyparts/arm_rightarm.cpp"
 #include "bodyparts/leftarm_playerkindleftarm.cpp"
-#include "bodyparts/groin_playerkindgroin.cpp"
+#include "bodyparts/rightarm_playerkindrightarm.cpp"
+
+#include "bodyparts/leg_leftleg.cpp"
+#include "bodyparts/leg_rightleg.cpp"
 #include "bodyparts/rightleg_playerkindrightleg.cpp"
 #include "bodyparts/leftleg_playerkindleftleg.cpp"
-#include "bodyparts/normaltorso_magicmushroomtorso.cpp"
-#include "bodyparts/normaltorso_dogtorso.cpp"
-#include "bodyparts/dogtorso_blinkdogtorso.cpp"
-#include "bodyparts/normaltorso_mysticfrogtorso.cpp"
-#include "bodyparts/largetorso_lobhsetorso.cpp"
+
+#include "bodyparts/groin_playerkindgroin.cpp"
+
+#include "bodyparts/torso_normaltorso.cpp"
+#include "bodyparts/torso_humanoidtorso.cpp"
+#include "bodyparts/humanoidtorso_playerkindtorso.cpp"
+
 #include "bodyparts/normaltorso_battorso.cpp"
+#include "bodyparts/normaltorso_dogtorso.cpp"
+#include "bodyparts/normaltorso_eddytorso.cpp"
+#include "bodyparts/normaltorso_largetorso.cpp"
+#include "bodyparts/normaltorso_magicmushroomtorso.cpp"
+#include "bodyparts/normaltorso_mysticfrogtorso.cpp"
 #include "bodyparts/normaltorso_spidertorso.cpp"
+
+#include "bodyparts/corpse_largecorpse.cpp"
+
+#include "bodyparts/dogtorso_blinkdogtorso.cpp"
+
+#include "bodyparts/largetorso_lobhsetorso.cpp"
 #include "bodyparts/largetorso_menatrixtorso.cpp"
