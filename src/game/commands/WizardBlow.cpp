749b641bd9831091cc5e385b62a866dc3acf3046
ketmar <ketmar@ketmar.no-ip.org>
2012-06-03 13:06:52 +0300

    'wizard blow' now does nothing

--- src/game/commands/WizardBlow.cpp
+++ src/game/commands/WizardBlow.cpp
@@ -1,6 +1,3 @@
 COMMAND(WizardBlow) {
-  if (!game::Wish(PLAYER, "%s appears from nothing!", "Two %s appear from nothing!", true)) {
-    ADD_MESSAGE("You changed your mind, didn't you, %s?", game::Insult());
-  }
   return false;
 };
