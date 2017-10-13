5d319121102a7230a1adcc97432ea271e4e592fe
Ketmar Dark <ketmar@ketmar.no-ip.org>
2017-10-14 00:47:14 +0300

    fix for https://github.com/Attnam/ivan/issues/110

--- src/game/items/scroll_scrollofgolemcreation.cpp
+++ src/game/items/scroll_scrollofgolemcreation.cpp
@@ -9,32 +9,24 @@ ITEM(scrollofgolemcreation, scroll)
 #else
 
 
-
-void scrollofgolemcreation::FinishReading(character* Reader)
-{
-  for(;;)
-  {
+void scrollofgolemcreation::FinishReading (character* Reader) {
+  for (;;) {
     itemvector Item;
 
-    if(!Reader->SelectFromPossessions(Item, CONST_S("Which item do you wish to use for golem creation?"), NO_MULTI_SELECT, &item::IsDestroyable))
-    {
+    if (!Reader->SelectFromPossessions(Item, CONST_S("Which item do you wish to use for golem creation?"), NO_MULTI_SELECT, &item::IsDestroyable)) {
       ADD_MESSAGE("You notice you haven't got anything to change into a golem.");
       return;
     }
 
-    if(!Item.empty())
-    {
+    if (!Item.empty()) {
       material* Main = Item[0]->GetMainMaterial();
       material* Sec = Item[0]->GetSecondaryMaterial();
       truth MainPossible = Main->GetCategoryFlags() & IS_GOLEM_MATERIAL;
-      truth SecPossible = Sec && Sec->GetVolume()
-        && Sec->GetCategoryFlags() & IS_GOLEM_MATERIAL
-        && !Sec->IsSameAs(Main);
+      truth SecPossible = Sec && Sec->GetVolume() && Sec->GetCategoryFlags() & IS_GOLEM_MATERIAL && !Sec->IsSameAs(Main);
 
-      if(!MainPossible && !SecPossible)
-      {
-  ADD_MESSAGE("You can't use that for golem creation.");
-  continue;
+      if (!MainPossible && !SecPossible) {
+        ADD_MESSAGE("You can't use that for golem creation.");
+        continue;
       }
 
       if (MainPossible && SecPossible) {
@@ -43,35 +35,29 @@ void scrollofgolemcreation::FinishReading(character* Reader)
 
       int MaterialConfig = MainPossible ? Main->GetConfig() : Sec->GetConfig();
       golem* Golem = golem::Spawn(MaterialConfig);
-      sLong Volume = MainPossible ? Sec && Sec->IsSameAs(Main)
-        ? Main->GetVolume() + Sec->GetVolume()
-        : Main->GetVolume() : Sec->GetVolume();
+      sLong Volume = (MainPossible ? (Sec && Sec->IsSameAs(Main) ? Main->GetVolume() + Sec->GetVolume() : Main->GetVolume()) : Sec->GetVolume());
       Golem->SetItemVolume(Volume);
       v2 Where = GetLevel()->GetNearestFreeSquare(Golem, Reader->GetPos());
+      if (itemcontainer* Container = dynamic_cast<itemcontainer*>(Item[0])) Container->GetContained()->MoveItemsTo(Reader->GetStack());
       Item[0]->RemoveFromSlot();
       Item[0]->SendToHell();
 
-      if(Where == ERROR_V2)
-      {
-  ADD_MESSAGE("You cast the spell and %s is sucked into a rainbow-coled magical vortex, but nothing happens.", Item[0]->CHAR_NAME(DEFINITE));
-  delete Golem;
+      if (Where == ERROR_V2) {
+        ADD_MESSAGE("You cast the spell and %s is sucked into a rainbow-coled magical vortex, but nothing happens.", Item[0]->CHAR_NAME(DEFINITE));
+        delete Golem;
+      } else {
+        ADD_MESSAGE("You cast the spell and %s is sucked into a rainbow-coled magical vortex.", Item[0]->CHAR_NAME(DEFINITE));
+        Golem->SetTeam(Reader->GetTeam());
+        Golem->PutTo(Where);
+
+        if (Golem->CanBeSeenByPlayer()) ADD_MESSAGE("Suddenly %s materializes!", Golem->CHAR_NAME(INDEFINITE));
+
+        Golem->GetLSquareUnder()->DrawParticles(RED);
       }
-      else
-      {
-  ADD_MESSAGE("You cast the spell and %s is sucked into a rainbow-coled magical vortex.", Item[0]->CHAR_NAME(DEFINITE));
-  Golem->SetTeam(Reader->GetTeam());
-  Golem->PutTo(Where);
-
-  if(Golem->CanBeSeenByPlayer())
-    ADD_MESSAGE("Suddenly %s materializes!", Golem->CHAR_NAME(INDEFINITE));
-
-  Golem->GetLSquareUnder()->DrawParticles(RED);
-      }
-
       break;
-    }
-    else if(game::TruthQuestion(CONST_S("Really cancel read? [y/N]")))
+    } else if (game::TruthQuestion(CONST_S("Really cancel read? [y/N]"))) {
       return;
+    }
   }
 
   RemoveFromSlot();
